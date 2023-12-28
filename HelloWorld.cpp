0951a72a723995c9882878355c81a611106fe44c
Sachin Shendge <shendgesachin01@gmail.com>
2023-12-28 15:07:56 +0530

    Change

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main(){
     std::cout<<"Hello World !!"<<std::endl;
     std::cout<<"Happy New Year !!"<<std::endl;
+     std::cout<<"Happy Diwali !!"<<std::endl;
+    return 0;
     return 0;
 }
